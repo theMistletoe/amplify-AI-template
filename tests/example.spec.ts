9902cf5ea85c54dec2c3020b0e5dd8a2b0431afe
theMistletoe <katachiwotodomete@gmail.com>
2024-12-06 22:11:16 +0900

    Refactor e2e directory and files

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,14 +1,21 @@
 import { test, expect } from '@playwright/test';
 
-test('has heading', async ({ page }) => {
+test.beforeEach(async ({ page }) => {
   await page.goto('https://main.d81yyylt2bnpi.amplifyapp.com/');
+  await page.getByRole('button', { name: 'Delete All Todos' }).click();
+  await expect(page.getByText('My first todo')).not.toBeVisible();
+});
 
+test.afterEach(async ({ page }) => {
+  await page.getByRole('button', { name: 'Delete All Todos' }).click();
+  await expect(page.getByText('My first todo')).not.toBeVisible();
+});
+
+test('has heading', async ({ page }) => {
   await expect(page.getByText('My todos')).toBeVisible();
 });
 
 test('add todo and delete all todos', async ({ page }) => {
-  await page.goto('https://main.d81yyylt2bnpi.amplifyapp.com/');
-
   await page.getByPlaceholder('Add a new todo').fill('My first todo');
   await page.getByRole('button', { name: '+ new' }).click();
 
