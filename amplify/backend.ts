29acd815ba7b7fca4145f5fe6cd0524573f395b8
theMistletoe <katachiwotodomete@gmail.com>
2024-12-06 23:27:26 +0900

    Refactor amplify.yml to remove e2e test commands and playwright dependency

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,8 +1,13 @@
+import { Tags } from 'aws-cdk-lib';
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
 
-defineBackend({
+const backend = defineBackend({
   auth,
   data,
 });
+
+
+const tags = Tags.of(backend.stack);
+tags.add('project', 'amplify-test-app');
